78dfc52c0d760591bbc1ce271d6ac19d80022d20
vaarigupta <vaarigupta24@gmail.com>
2018-08-31 00:48:06 +0530

    modules added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+console.log("STARTING app.js");
 
 const fs = require('fs');
 const os = require('os');
+const note = require("./notes.js");
+const _ = require('lodash');
 
-console.log("helllllllllooooo");
+
 // fs.appendFile('intro.txt', "Helloo world",function(err){
 // 	if(err)
 // 	{
@@ -10,9 +13,53 @@ console.log("helllllllllooooo");
 // 	}
 // });
 
+// appendFile method is just appending the content in the file .It takes three arguments
+// 1- file name 
+// 2- content 
+// 3- function if error comes
 
-let user = os.userInfo();
+// let user = os.userInfo();
 // console.log(user);
-fs.appendFileSync('intro.txt',"helloo" +user.username + "!!!");
-fs.appendFileSync('intro.txt',`helloo ${user.username} !!!`);
+// fs.appendFileSync('intro.txt',"helloo" +user.username + "!!!");
+
+//using appendFileSync , there is no need to use 3rd argument
+//fs.appendFileSync('intro.txt',`helloo ${user.username} !!! .you are ${note.age} years old`);
+
+//intead of concatenating the string , we can use template string in which we have to use ${} for evaluating any value for a variable 
+// fs.appendFileSync('notes.js',`console.log("STARTING Note.js")`);
+
+// let print = note.fun();
+// console.log(print);
+
+// let add = note.add(2,3); // we are using add function contained in the note.js by importing note here
+// console.log(add);
+// let add1 = note.add("helloo "," vaari");
+// console.log(add1);
+
+// console.log(_.isString('yes'));
+// console.log(_.isString(true));//tells about the given input is a string or not
+// console.log(_.uniq([1,2,3,3,4,5,5,2,2,4,4,4,4 , 3,78,89,789])); // it gives the unique elements of an array passed to it
+// //console.log(process) -gives 
+//console.log(process.argv)
+var command = process.argv[2]
+if(command === 'list')
+{
+	console.log("Listing notes")
+}
+else if(command === 'add')
+{
+	console.log('adding notes')
+}
+else if(command === 'remove')
+{
+	console.log('removing notes')
+}
+else if(command === 'read')
+{
+	console.log('reading my notes')
+}
+else
+{
+	console.log('not recognisable')
+}
 
---
+++ notes.js
@@ -0,0 +1,13 @@
+console.log("STARTING Note.js");
+// console.log(module);
+
+module.exports.age = 21;
+module.exports.fun = function()
+{
+	
+	return "Hey vaari";
+}
+
+module.exports.add = function(a,b){
+  return (a+b)
+}
