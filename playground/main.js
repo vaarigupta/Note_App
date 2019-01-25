63222b62c283a868d95b573d591039cc23707554
vaarigupta <vaarigupta24@gmail.com>
2019-01-25 23:51:17 +0530

    practice

--- playground/main.js
+++ playground/main.js
@@ -1,29 +1,56 @@
 const fs = require('fs');
-const os = require('os');
+const yargs = require('yargs');
 const prac = require('./practice.js');
 //const user = os.userInfo();
 //console.log(user);
 // let ans = prac.addNum(2,3);
 // console.log(ans);
 
-var command = process.argv[2];
-console.log("Command : "+command);
-if(command === 'add')
-{
-  console.log("Adding notes");
+//var command = process.argv;
+// var argv = yargs.argv;
+// var command = argv._[0];
+// console.log("yargs : ",argv);
+//
+// console.log("Command : "+command);
+// if(command === 'add')
+// {
+//
+//   prac.addNote(argv.title , argv.body);
+// }
+// else if( command === 'list')
+// {
+//
+//   prac.getAll();
+// }
+// else if(command === 'read')
+// {
+//
+//   prac.readNote(argv.title);
+// }
+// else if (command === 'remove')
+// {
+//
+//   prac.removeNote(argv.title);
+// }
+// else {
+//   console.log("Sorry Not recognized");
+// }
+
+
+var object = {
+  title: 'vaari',
+  body: 'she is a good girl'
 }
-else if( command === 'list')
-{
-  console.log("Listing all Notes");
-}
-else if(command === 'read')
-{
-  console.log("fetching all nodes");
-}
-else if (command === 'remove')
-{
-  console.log("Removing All notes");
-}
-else {
-  console.log("Sorry Not recognized");
-}
+/// Object to JSON String
+var ObjJson = JSON.stringify(object);
+
+/// writing content(JSON STRING) to file
+fs.writeFileSync('json1.json',ObjJson);
+
+/// Read Content of the file and save it to readObj
+var readObj = fs.readFileSync('json1.json');
+
+/// Again convert JSON to Object
+var jsonToObj = JSON.parse(readObj);
+console.log(typeof jsonToObj);
+console.log(jsonToObj.title);
--- playground/practice.js
+++ playground/practice.js
@@ -1,6 +1,42 @@
 
 
 
-module.exports.addNum = (a,b)=>{
-  return a+b;
+// module.exports.addNum = (a,b)=>{
+//   return a+b;
+// }
+
+
+var addNote = (title , body)=>{
+  console.log(`Adding Notes :
+Title : ${title}
+Body : ${ body}
+    `);
+}
+
+var getAll = ()=>
+{
+  console.log(`Getting all Notes`);
+}
+
+var readNote = (title)=>{
+  console.log(`Reading Notes :
+Title : ${title}`)
+}
+
+
+var removeNote = (title)=>{
+
+console.log(`Removing Notes :
+Title : ${title}  `)
+
+}
+
+
+
+module.exports = {
+
+  addNote,
+  getAll,
+  readNote,
+  removeNote
 }
