bbf695514fb68cacf441b80ee0e6ada2b9920342
vaarigupta <vaarigupta24@gmail.com>
2019-01-27 21:33:00 +0530

    modified

--- playground/main.js
+++ playground/main.js
@@ -6,51 +6,50 @@ const prac = require('./practice.js');
 // let ans = prac.addNum(2,3);
 // console.log(ans);
 
-//var command = process.argv;
-// var argv = yargs.argv;
-// var command = argv._[0];
-// console.log("yargs : ",argv);
-//
-// console.log("Command : "+command);
-// if(command === 'add')
-// {
-//
-//   prac.addNote(argv.title , argv.body);
-// }
-// else if( command === 'list')
-// {
-//
-//   prac.getAll();
-// }
-// else if(command === 'read')
-// {
-//
-//   prac.readNote(argv.title);
-// }
-// else if (command === 'remove')
-// {
-//
-//   prac.removeNote(argv.title);
-// }
-// else {
-//   console.log("Sorry Not recognized");
-// }
+var command = process.argv;
+var argv = yargs.argv;
+var command = argv._[0];
+//console.log("yargs : ",argv);
 
-
-var object = {
-  title: 'vaari',
-  body: 'she is a good girl'
+console.log("Command : "+command);
+if(command === 'add')
+{
+  prac.addNote(argv.title , argv.body);
 }
-/// Object to JSON String
-var ObjJson = JSON.stringify(object);
+else if( command === 'list')
+{
 
-/// writing content(JSON STRING) to file
-fs.writeFileSync('json1.json',ObjJson);
+  prac.getAll();
+}
+else if(command === 'read')
+{
 
-/// Read Content of the file and save it to readObj
-var readObj = fs.readFileSync('json1.json');
+  prac.readNote(argv.title);
+}
+else if (command === 'remove')
+{
 
-/// Again convert JSON to Object
-var jsonToObj = JSON.parse(readObj);
-console.log(typeof jsonToObj);
-console.log(jsonToObj.title);
+  prac.removeNote(argv.title);
+}
+else {
+  console.log("Sorry Not recognized");
+}
+
+
+// var object = {
+//   title: 'vaari',
+//   body: 'she is a good girl'
+// }
+// /// Object to JSON String
+// var ObjJson = JSON.stringify(object);
+//
+// /// writing content(JSON STRING) to file
+// fs.writeFileSync('json1.json',ObjJson);
+//
+// /// Read Content of the file and save it to readObj
+// var readObj = fs.readFileSync('json1.json');
+//
+// /// Again convert JSON to Object
+// var jsonToObj = JSON.parse(readObj);
+// console.log(typeof jsonToObj);
+// console.log(jsonToObj.title);
--- playground/practice.js
+++ playground/practice.js
@@ -5,12 +5,24 @@
 //   return a+b;
 // }
 
-
+const fs = require('fs');
 var addNote = (title , body)=>{
-  console.log(`Adding Notes :
-Title : ${title}
-Body : ${ body}
-    `);
+console.log("Adding Notes ");
+var notes = [];
+var note = {
+  title,
+  body
+}
+notes.push(noteToStr);
+var noteToStr = JSON.stringify(notes);
+fs.writeFileSync('json1.json',noteToStr);
+var StrToNotes = fs.readFileSync('json1.json');
+
+var notesFromFile = JSON.parse(StrToNotes);
+
+console.log(notesFromFile);
+
+
 }
 
 var getAll = ()=>
